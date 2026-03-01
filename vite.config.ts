56e665ea75e079d12913208b45470983e29488d8
youhei usami <usao9110@icloud.com>
2026-03-01 13:32:59 +0400

    3/1

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5177, // ← 好きなポート番号に変更
+  },
+});
