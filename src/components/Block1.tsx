2b575de670474354a21a0f07ca0bbb965676e19f
youhei usami <usao9110@icloud.com>
2026-03-02 11:19:09 +0400

    3/2

--- src/components/Block1.tsx
+++ src/components/Block1.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Block1 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className="introduction"></section>
+      <h1>Block1</h1>
+    </div>
+  );
 };
 
 export default Block1;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Block1 from "./components/Block1.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Block1 />
   </StrictMode>,
-)
+);
