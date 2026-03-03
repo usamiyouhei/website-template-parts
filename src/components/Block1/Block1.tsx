757204104f4a58b2cbdd99c31282ae59b8271efc
youhei usami <usao9110@icloud.com>
2026-03-03 14:35:08 +0400

    3/3

--- src/components/Block1/Block1.tsx
+++ src/components/Block1/Block1.tsx
@@ -5,8 +5,26 @@ import styles from "./Block1.module.scss";
 const Block1 = () => {
   return (
     <div>
-      <section className={styles.wrapper}></section>
-      <h1>Block1</h1>
+      <section className={styles.introduction}>
+        <div className={styles.back}>
+          <p>
+            Partnership<span className="sp-none"></span>
+            <br className="sp-disblock" />
+            Platform
+          </p>
+        </div>
+        <div className="container">
+          <div className="thumb">
+            <div className="image">
+              <h1>Hello</h1>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/style.scss";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import Block1 from "./components/Block1/Block1.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     <Block1 />
   </StrictMode>,
 );
