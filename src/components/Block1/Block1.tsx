f6542e6b1b7928e8871517f4138ba5efda383527
youhei usami <usao9110@icloud.com>
2026-03-06 13:07:30 +0400

    3/6

--- src/components/Block1/Block1.tsx
+++ src/components/Block1/Block1.tsx
@@ -29,10 +29,44 @@ const Block1 = () => {
                 <h3>
                   <div className="catch">introduction of mediplus</div>
                 </h3>
+                <h2>
+                  <div className="line">
+                    <div className="catch">美容医療専門</div>
+                  </div>
+                  <div className="line">
+                    <div className="catch">オンライン診療</div>
+                  </div>
+                </h2>
               </div>
-              <div className="right"></div>
+              <div className="right">
+                <div className="mark_wrapper">
+                  <ul>
+                    <li>ダイエットGLP-1</li>
+                    <li>美容内服液</li>
+                    <li>AGA/FAGA治療</li>
+                    <li>エクソソーム</li>
+                    <li>ゼオスキン</li>
+                    <li>医療マツゲ育毛</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div className="sentence"></div>
+            <div className="sentence">
+              <p>
+                株式会社Supercellが運営する美容医療オンライン診療プラットフォームです。お客様に「もっと手軽に美しくなる」ための美容医療メニューの診療でき、あらゆる処方品でお客様のお悩みを解決いたします。
+              </p>
+              <p>
+                オンライン診療をご希望されているお客様は、MEDIPLUS提携医師の指導・アドバイスのもと処方が決定いたしましたら、
+                <span className="big bold">自宅にお薬をお届け</span>します。
+              </p>
+              <p>
+                メディプラス独自の厳選した美容医療、お薬、処方品をお手軽に利用することができ、
+                <span className="big bold">
+                  一度利用した美容意識の高いお客様から特にご好評
+                </span>
+                頂いています。
+              </p>
+            </div>
           </div>
         </div>
       </section>
